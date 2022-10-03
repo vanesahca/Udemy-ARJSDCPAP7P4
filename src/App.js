1fbbdf882caece8f3f76092ceab31d1db743ed4b
Vanesa Hernandez <vanesahca@gmail.com>
2022-10-03 12:26:13 -0500

    First changes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import CharacterContainer from "./components/CharacterContainer";
+import Container from "./components/Container";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import { useState } from "react";
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch(
+      "https://simpsons-quotes-api.herokuapp.com/quotes?count=3"
+    );
+    const frase = await api.json();
+    setCharacters(frase);
+  };
+
+  return (
+    <Container>
+      <Header />
+      {!characters ? (
+        <Welcome reqApi={reqApi} />
+      ) : (
+        <CharacterContainer characters={characters} reqApi={reqApi} />
+      )}
+    </Container>
+  );
+}
+
+export default App;
